252cb8787883ba58bb7418f7a20c518447ef72c6
Seth Cleveland <scleveland@zenoss.com>
2013-06-03 17:36:14 -0500

    adding fedora support

--- recipes/package.rb
+++ recipes/package.rb
@@ -30,6 +30,7 @@ else
   python_pkgs = value_for_platform_family(
                   "debian" => ["python","python-dev"],
                   "rhel" => ["python","python-devel"],
+                  "fedora" => ["python","python-devel"],
                   "freebsd" => ["python"],
                   "smartos" => ["python27"],
                   "default" => ["python","python-dev"]
